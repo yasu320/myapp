a60afd62724cc696f3681636c3a9fa7ada5f9796
yasu320 <yasu320.musashi@gmail.com>
2019-04-14 19:48:22 +0900

    Introduce Devise gem and Add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get "/signup" => "devise/registrations#new", as: "new_user_registration"
+  end
+
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  root to: "home#index"
 end
